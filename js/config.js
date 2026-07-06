feb1b4e673121834ff50dfb0c7e847b04dce72ce
tcheriktchertilov <137436873+tcheriktchertilov@users.noreply.github.com>
2026-07-06 15:21:24 +0900

    config.js

--- js/config.js
+++ js/config.js
@@ -1,20 +1,18 @@
 const SERVER_CONFIG = {
     name: 'LYRA',
     fullName: 'LYRA 3 сезон | Minecraft Server',
+    ip: 'Lyra.minerent.io',
     version: '1.21.11',
     maxPlayers: 100,
-
     social: {
         telegram: 'https://t.me/lyraminecraft',
         discord: 'https://discord.gg/s6uxptuxJY'
     },
-
     features: [
         { icon: 'fa-chart-line', title: 'Система роста', desc: 'Прогрессируйте, открывайте новые возможности и выростайте.' },
         { icon: 'fa-paw', title: 'Система животных', desc: 'Новая система животных, приручение и уникальные зачарования.' },
         { icon: 'fa-crown', title: 'Гильдии', desc: 'Объединяйтесь с друзьями, играйте вместе, стройте города и учавствуйте в событиях!' }
     ],
-
     getStatus: function() {
         return {
             online: true,
